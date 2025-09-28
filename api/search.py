58e11615064fa2167077a5b2682ffc45b0c8bde0
ethanreesewagner <73082851+ethanreesewagner@users.noreply.github.com>
2025-09-28 14:17:02 -0700

    Update search.py

--- api/search.py
+++ api/search.py
@@ -6,7 +6,6 @@ config=dotenv_values(".env")
 client = OpenAI(api_key=config["OPENAI_API_KEY"])
 pc = Pinecone(api_key=config["PINECONE_API_KEY"])
 index = pc.Index("testingvectors")
-
 def search(query,file_path):
     # Add a filter to only return results where the metadata "title" matches a specific value.
     # For demonstration, we'll prompt the user for the title to filter by.
